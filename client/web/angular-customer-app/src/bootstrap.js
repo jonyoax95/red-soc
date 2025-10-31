8d15f834e121b27221a9a8e7143bcca8d2fcad39
Firebase Studio <noreply@firebase.studio>
2025-10-31 22:09:55 +0000

    Initialized workspace with Firebase Studio

---
+++ client/web/angular-customer-app/src/bootstrap.js
@@ -0,0 +1,11 @@
+window["APP_TEMPLATE_BOOTSTRAP"] = {
+
+    firebase: {
+      apiKey: "AIzaSyC3SuUq7CfrM2ahKn_90TKV4vK07guF5pw",
+      authDomain: "pruebas-5144a.firebaseapp.com",
+      projectId: "pruebas-5144a",
+      storageBucket: "pruebas-5144a.firebasestorage.app",
+      messagingSenderId: "596257783887",
+      appId: "1:596257783887:web:7366d8721515bf2ac82a3a",
+    }
+    };
